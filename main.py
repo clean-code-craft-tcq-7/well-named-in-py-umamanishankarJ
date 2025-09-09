281fd93aba9766348b48239e37410a593c573f47
umamanishankarJ <j.u.m.shankar1213@gmail.com>
2025-09-09 10:06:49 +0530

    Update main.py
    update to new function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from colourpair_testing import run_all_tests
+from colour_print import print_color_code_manual
 
 if __name__ == '__main__':
     run_all_tests()
-
+    print("\nReference Manual for Color Coding:\n")
+    print(print_color_code_manual())
